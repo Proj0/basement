0800c853c2013ecdeb21636714ead257cefd219a
user <yyff1225257758@gmail.com>
2015-04-17 11:40:55 +0800

    235235

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :body, type: String
+  field :Clicks, type: Integer, default: 0
+
+ scope :hottest, proc {where(:Clicks.gt => 5).desc}
+ field :replies_count, type: Integer, default: 0
+
+  has_many :replies, dependent: :destroy
+end
+
---
+++ app/models/reply.rb
@@ -0,0 +1,13 @@
+class Reply
+	include Mongoid::Document
+	include Mongoid::Timestamps
+
+	field :body, type: String, default: ""
+	belongs_to :post
+
+	validates_presence_of :post
+
+	after_create do 
+		post.inc(:replies_count => 1)
+	end
+end
